99938c748d59162bf92f6f77c3a1529db3528fcd
Taffer <chrish@pobox.com>
2021-03-28 13:12:36 -0400

    Fix pngbrute bugs

--- pngbrute.py
+++ pngbrute.py
@@ -13,17 +13,12 @@ class Crusher(threading.Thread):
     def __init__(self, filename, level):
         threading.Thread.__init__(self)
         self.name = '{0}-{1}'.format(filename, level)
+        self.crushed_name = self.name + '.crushed'
         self.filename = filename
         self.level = level
 
     def run(self):
-        crushed_name = '{1}-{0}.crushed'.format(self.level, self.filename)
-        print('Crushing {1} for level {0} -> {2}'.format(self.level, self.filename, crushed_name))
-        os.system('pngcrush -brute -l {0} {1} {2} > /dev/null 2>&1'.format(self.level, self.filename, crushed_name))
-        orig_size = os.path.getsize(self.filename)
-        new_size = os.path.getsize(crushed_name)
-        if new_size == 0 or new_size >= orig_size:
-            os.unlink(crushed_name)
+        os.system('pngcrush -brute -l {0} {1} {2} > /dev/null 2>&1'.format(self.level, self.filename, self.crushed_name))
 
 
 def crush(filename):
@@ -36,20 +31,25 @@ def crush(filename):
 
     orig_size = os.path.getsize(filename)
     best_size = orig_size
-    winner = 0
-    for z in range(1, 10):
-        crushed_name = '{0}-{1}.crushed'.format(filename, z)
-        crushed_size = os.path.getsize(crushed_name)
-        if crushed_size > 0 and crushed_size < best_size:
-            if winner != 0:
-                os.unlink('{0}-{1}.crushed'.format(filename, winner))
-            winner = z
+    winner = None
+
+    for c in crushers:
+        crushed_size = os.path.getsize(c.crushed_name)
+        if crushed_size == 0:
+            os.unlink(c.crushed_name)
+            continue
+
+        if crushed_size < best_size:
+            if winner is not None:
+                os.unlink(winner.crushed_name)
+            winner = c
             best_size = crushed_size
         else:
-            os.unlink(crushed_name)
+            os.unlink(c.crushed_name)
 
-    shutil.move(filename, filename + '.orig')
-    os.rename('{0}-{1}.crushed'.format(filename, winner), filename)
+    if winner is not None:
+        shutil.move(filename, filename + '.orig')
+        os.rename(winner.crushed_name, filename)
 
 
 def main(args):
